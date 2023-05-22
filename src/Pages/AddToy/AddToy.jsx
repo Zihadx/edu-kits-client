e98e0ed6dda63ffba3ca1cf461909d4fd3f24184
zihadx <blankzihad@gmail.com>
2023-05-22 20:49:11 +0600

    get data from input field

--- src/Pages/AddToy/AddToy.jsx
+++ src/Pages/AddToy/AddToy.jsx
@@ -1,8 +1,49 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect } from "react";
+
 const AddToy = () => {
+  const handleAddToy = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.title.value;
+    const seller_name = form.name.value;
+    const email = form.email.value;
+    const rating = form.rating.value;
+    const price = form.price.value;
+    const available_quantity = form.quantity.value;
+    const subcategory = form.category.value;
+    const image_url = form.photo.value;
+    const description = form.description.value;
+
+    const addNewToy = {
+      name,
+      seller_name,
+      email,
+      rating,
+      price,
+      available_quantity,
+      subcategory,
+      image_url,
+      description,
+    };
+    console.log(addNewToy);
+
+    fetch('http://localhost:5000/toys',{
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(addNewToy)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  };
   return (
     <div className="px-4 md:px-24 mt-8">
       <div className="card-body bg-base-200 rounded-xl md:12 lg:p-16">
-        <form>
+        <form onSubmit={handleAddToy}>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="form-control">
               <label className="label">
@@ -68,7 +109,9 @@ const AddToy = () => {
 
             <div className="form-control">
               <label className="label">
-                <span className="label-text font-bold">Category (Math Learning, Science Kits, Engineering kits)</span>
+                <span className="label-text font-bold">
+                  Category (Math Learning, Science Kits, Engineering kits)
+                </span>
               </label>
               <input
                 type="text"
