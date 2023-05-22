51329386ab09823bdb84f8a3e52377cb51f1027a
zihadx <blankzihad@gmail.com>
2023-05-23 03:30:27 +0600

    get my toy data from database

--- src/Pages/MyToy/MyToy.jsx
+++ src/Pages/MyToy/MyToy.jsx
@@ -1,8 +1,19 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Providers/AuthProviders";
 
 const MyToy = () => {
+    const {user} = useContext(AuthContext)
+    const [toys, setToys]= useState([])
+    useEffect(()=>{
+        fetch(`http://localhost:5000/mytoy/${user?.email}`)
+        .then(res=> res.json())
+        .then(data=> {
+            setToys(data)
+        })
+    },[user])
     return (
         <div>
-            <h1>my toy</h1>
+            <h1>my toy: {toys.length}</h1>
         </div>
     );
 };
